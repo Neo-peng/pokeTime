a20ea922a3d1b2ece36ba26e8d0c75b8324a6b19
Neo-peng <jp170518@gmail.com>
2019-09-28 16:15:25 +1000

    route params

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
 import {Route, BrowserRouter} from "react-router-dom"
+import Post from "./components/Post.js"
 
 class App extends React.Component {
   render () {
@@ -14,6 +15,7 @@ class App extends React.Component {
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/contact" component={Contact}/>
+            <Route path="/:post_id" component={Post}/>
         </div>
       </BrowserRouter>
         
---
+++ src/components/Post.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: null,
+    }
+  }
+  componentDidMount() {
+    console.log(this.props)
+    const id = this.props.match.params.post_id
+    this.setState({
+      id: id,
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h4 className="center">{this.state.id}</h4>
+      </div>
+    )
+  }
+}
+
+export default Post;
