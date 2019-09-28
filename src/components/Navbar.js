2f0d12f77509a46f452e084ef92c4c158e84fe54
Neo-peng <jp170518@gmail.com>
2019-09-28 14:13:36 +1000

    v2-NavLink

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {NavLink} from "react-router-dom"
+
+function Navbar() {
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Poke' Times</a>
+        <ul className="right">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+ 
+export default Navbar
